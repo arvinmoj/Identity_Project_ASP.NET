8c36ae8af732da0fc3c33062e5fb9f4c74e52283
arvinmoj <52661749+arvinmoj@users.noreply.github.com>
2021-02-14 15:08:52 +0330

    Update_scaffolding

--- My_Application/Controllers/EmployeeController.cs
+++ My_Application/Controllers/EmployeeController.cs
@@ -54,7 +54,7 @@ namespace My_Application.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Firstname,Lastname,Gender")] Employee employee)
+        public async Task<IActionResult> Create([Bind("Id,Firstname,Lastname,City,Gender")] Employee employee)
         {
             if (ModelState.IsValid)
             {
@@ -87,7 +87,7 @@ namespace My_Application.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Firstname,Lastname,Gender")] Employee employee)
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Firstname,Lastname,City,Gender")] Employee employee)
         {
             if (id != employee.Id)
             {
