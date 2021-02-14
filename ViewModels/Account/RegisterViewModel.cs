a2ba8581ebc40edd72183915dfeab62e07000d7d
arvinmoj <52661749+arvinmoj@users.noreply.github.com>
2021-02-14 15:02:05 +0330

    Create_AccountController_MyApplication Create_Register_View_MyAoolication Add_RegisterViewModel_ViewModels

---
+++ ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        public RegisterViewModel() : base()
+        {
+        }
+
+        [MaxLength(64)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Username Is Required")]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        [MaxLength(64)]
+        [Display(Name = "EmailAddress")]
+        [Required(ErrorMessage = "Email Is Required")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail Is Not Valid")]
+        public string Email { get; set; }
+
+        [MaxLength(64)]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password Is Required")]
+        public string Password { get; set; }
+
+        [MaxLength(64)]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        [Display(Name = "ConfirmPassword")]
+        [Required(ErrorMessage = "ConfirmPassword Is Required")]
+        public string ConfirmPassword { get; set; }
+    }
+}
