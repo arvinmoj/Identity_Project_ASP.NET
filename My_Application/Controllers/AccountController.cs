7db886c5d7253755e5a9e60076e7847755567253
macbook <arvinmojaverian@hotmail.com>
2021-02-17 17:22:24 +0330

    Login

--- My_Application/Controllers/AccountController.cs
+++ My_Application/Controllers/AccountController.cs
@@ -1,16 +1,117 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
+using ViewModels.Account;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace My_Application.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                                 SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel register)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = register.Username,
+                    Email = register.Email,
+                    EmailConfirmed = true,
+                };
+
+                var result = await _userManager.CreateAsync(user, register.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var identityError in result.Errors)
+                {
+                    ModelState.AddModelError("", identityError.Description);
+                }
+            }
+
+            return View(register);
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewData["returnUrl"]= returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel login , string returnUrl = null)
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Index", "Home");
+            } 
+            
+            ViewData["returnUrl"]= returnUrl;
+
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(login.Username,
+                                                                       login.Password,
+                                                                       login.RememberMe,
+                                                                       true);
+
+                if (result.Succeeded)
+                {
+                    if(!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+
+                if (result.IsLockedOut)
+                {
+                    ViewData["ErrorMessage"] = "You've been temporarily locked out of your account for 5 hours";
+                    return View(login);
+                }
+
+                ModelState.AddModelError("", "Your User Username Or Password Is Incorrect");
+            }
+            return View(login);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- My_Application/Startup.cs
+++ My_Application/Startup.cs
@@ -6,6 +6,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using ViewModels.Account;
+using Validator.Account;
 
 namespace My_Application
 {
@@ -22,7 +26,10 @@ namespace My_Application
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews()
-                .AddRazorRuntimeCompilation();
+                .AddRazorRuntimeCompilation()
+                .AddFluentValidation(); 
+
+            services.AddTransient<IValidator<RegisterViewModel>,RegisterValidation>();
 
             services.AddDbContext<DatabaseContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("DatabaseContext")));
@@ -30,6 +37,11 @@ namespace My_Application
             services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<DatabaseContext>()
                 .AddDefaultTokenProviders();
+
+        
+
+            //services.AddTransient<IValidator<RegisterValidation>, RegisterValidation>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
